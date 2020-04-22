9b9f0c6756af08978e0418a0175146a28890bf8c
amesaichu <amel00704@gmail.com>
2020-04-22 20:45:56 +0700

    ganti title

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body>
     <div>
-    <h1>Test</h1>
+    <h1>Testing</h1>
     <p>It is a long established fact that a reader will be distracted 
     by the readable content of a page when looking at its layout. 
     The point of using Lorem Ipsum is that it has a more-or-less normal 
